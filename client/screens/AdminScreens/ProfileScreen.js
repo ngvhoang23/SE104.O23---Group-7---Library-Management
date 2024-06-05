4555f45119bb0c4c10d4090366f3489a55f31339
1longg <lbnlong109@gmail.com>
2024-06-05 20:33:39 +0700

    feat(profile screen

---
+++ client/screens/AdminScreens/ProfileScreen.js
@@ -0,0 +1,64 @@
+import { StyleSheet, Text, View } from "react-native";
+import { useUserInfoContext } from "../../context/userInfoContext";
+import UserInfoTag from "../../components/UserInfoTag";
+import OptionProfileItem from "../../components/OptionProfileItem";
+import {
+  Ionicons,
+  Feather,
+  SimpleLineIcons,
+  EvilIcons,
+} from "@expo/vector-icons";
+import { normalize } from "../../defined_function";
+
+function ProfileScreen({ navigation }) {
+  const { user, setUser } = useUserInfoContext();
+
+  const { full_name, user_avatar, role, email_address } = user;
+
+  return (
+    <View style={styles.wrapper}>
+      <UserInfoTag
+        full_name={full_name}
+        email={email_address}
+        role={role}
+        user_avatar={user_avatar}
+        onEdit={() => navigation.navigate("Edit Profile")}
+      />
+      <View style={styles.optionContainer}>
+        <OptionProfileItem
+          _styles={styles.optionItem}
+          icon={
+            <Ionicons
+              name="settings-outline"
+              size={normalize(20)}
+              color="#6c60ff"
+            />
+          }
+          title={"Cài đặt"}
+          description={"Đổi mật khẩu, email,..."}
+          onPress={() => navigation.navigate("Settings")}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    width: "100%",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+
+  optionContainer: {
+    paddingTop: normalize(10),
+  },
+
+  optionItem: {
+    width: "100%",
+  },
+});
+
+export default ProfileScreen;
